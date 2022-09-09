9385a10710b3d7aef0824850561fe087fb42f32f
Leonardo Costa Santos <31082603+leonardocs1@users.noreply.github.com>
2022-09-09 09:32:42 -0300

    Declaração de tipos publica e privada

--- curso_java_web/src/cursojava/executavel/Teste.java
+++ curso_java_web/src/cursojava/executavel/Teste.java
@@ -7,6 +7,11 @@ public class Teste {
 	public static void main(String[] args) {
 		
 		Aluno aluno1 = new Aluno();
+		aluno1.nome = "João";
+		aluno1.idade = 50;
+		
+		System.out.println("Nome do aluno 1 é = " + aluno1.nome);
+		System.out.println("Idade é = " + aluno1.idade);
 		
 		Aluno aluno2 = new Aluno();
 
