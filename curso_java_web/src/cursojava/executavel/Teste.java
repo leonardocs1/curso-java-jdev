ffda73abe0c192e514da954e58f013312f1d84f2
Leonardo Costa Santos <31082603+leonardocs1@users.noreply.github.com>
2022-09-09 11:17:33 -0300

    Os metódos comuns Getters e Setters

--- curso_java_web/src/cursojava/executavel/Teste.java
+++ curso_java_web/src/cursojava/executavel/Teste.java
@@ -7,13 +7,31 @@ public class Teste {
 	public static void main(String[] args) {
 		
 		Aluno aluno1 = new Aluno();
-		aluno1.nome = "João";
-		aluno1.idade = 50;
 		
-		System.out.println("Nome do aluno 1 é = " + aluno1.nome);
-		System.out.println("Idade é = " + aluno1.idade);
+		aluno1.setNome("João da Silva");
+		aluno1.setIdade(50);
+		aluno1.setDataNascimento("18/10/1987");
+		aluno1.setRegistroGeral("4444.444.444");
+		aluno1.setNumeroCpf("223.455.455-34");
+		aluno1.setNomeMae("Shirlei");
+		aluno1.setNomePai("Antônio");
+		aluno1.setDataMatricula("10/01/2019");
+		aluno1.setSerieMatriculado("5");
+		aluno1.setNomeEscola("Escola JDEV Treinamento");
+		
+		System.out.println("Nome do aluno 1 é = " + aluno1.getNome());
+		System.out.println("Idade do aluno 1 é = " + aluno1.getIdade());
+		System.out.println("Data de Nascimento do aluno 1 é = " + aluno1.getDataNascimento());
 		
 		Aluno aluno2 = new Aluno();
+		
+		aluno2.setNome("Pedro");
+		aluno2.setIdade(40);
+		aluno2.setDataNascimento("10/10/1970");
+		
+		System.out.println("Nome do aluno 2 é = " + aluno2.getNome());
+		System.out.println("Idade do aluno 2 é = " + aluno2.getIdade());
+		System.out.println("Data de Nascimento do aluno 2 é = " + aluno2.getDataNascimento());
 
 		Aluno aluno3 = new Aluno();
 		
