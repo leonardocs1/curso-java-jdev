31a6acc76a9ba061d2957c1c5fbfe75745b5f9ce
Leonardo Costa Santos <31082603+leonardocs1@users.noreply.github.com>
2022-09-13 22:27:33 -0300

    Sintaxe correta da herança

---
+++ curso_java_web/src/cursojava/classes/Diretor.java
@@ -0,0 +1,33 @@
+package cursojava.classes;
+
+public class Diretor extends Pessoa {
+
+	private String registtroEducacao;
+	private int tempoDirecao;
+	private String titulacao;
+
+	public String getRegisttroEducacao() {
+		return registtroEducacao;
+	}
+
+	public void setRegisttroEducacao(String registtroEducacao) {
+		this.registtroEducacao = registtroEducacao;
+	}
+
+	public int getTempoDirecao() {
+		return tempoDirecao;
+	}
+
+	public void setTempoDirecao(int tempoDirecao) {
+		this.tempoDirecao = tempoDirecao;
+	}
+
+	public String getTitulacao() {
+		return titulacao;
+	}
+
+	public void setTitulacao(String titulacao) {
+		this.titulacao = titulacao;
+	}
+
+}
