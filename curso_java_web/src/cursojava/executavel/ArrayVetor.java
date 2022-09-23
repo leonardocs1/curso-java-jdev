2133ce9b3eb88dd1adc7358440676ee41be9da3f
Leonardo Costa Santos <31082603+leonardocs1@users.noreply.github.com>
2022-09-23 08:56:36 -0300

    Manipulando Array de Objetos

--- curso_java_web/src/cursojava/executavel/ArrayVetor.java
+++ curso_java_web/src/cursojava/executavel/ArrayVetor.java
@@ -8,48 +8,61 @@ import cursojava.classes.Disciplina;
 public class ArrayVetor {
 
 	public static void main(String[] args) {
-		
-		
-		double[] notas = {8.8, 9.7, 7.6, 6.8};
-		double[] notasLogica = {7.1, 5.7, 9.6, 7.8};
-		
+
+		double[] notas = { 8.8, 9.7, 7.6, 6.8 };
+		double[] notasLogica = { 7.1, 5.7, 9.6, 7.8 };
+
 		// Criação de alunos
 		Aluno aluno = new Aluno();
 		aluno.setNome("Alex Fernando Egidio");
 		aluno.setNomeEscola("JDEV Treinamento");
-		
+
 		// criação da disciplina
 		Disciplina disciplina = new Disciplina();
 		disciplina.setDisciplina("Curso de Java");
 		disciplina.setNotas(notas);
-		
+
 		aluno.getDisciplinas().add(disciplina);
-		
+
 		Disciplina disciplina2 = new Disciplina();
 		disciplina2.setDisciplina("Lógica de programação");
 		disciplina2.setNotas(notasLogica);
-		
+
 		aluno.getDisciplinas().add(disciplina2);
 		
-		System.out.println("Nome do aluno = " + aluno.getNome() + " inscrito no curso: " + aluno.getNomeEscola());
-		System.out.println("-----------Disciplina do aluno--------------");
-		for (Disciplina d : aluno.getDisciplinas()) {
-			System.out.println("Disciplina: " + d.getDisciplina());
-			
-			double notaMin = 0.0;			
-			for (int i = 0; i < d.getNotas().length; i++) {
-				System.out.println("Nota " + i + " é igual = " + d.getNotas()[i]);
-				if (i == 0) {
-					notaMin = d.getNotas()[i];
-				} else {
-					if (d.getNotas()[i] < notaMin) {
-						notaMin = d.getNotas()[i];
-					}
+		Aluno aluno2 = new Aluno();
+		aluno2.setNome("Alex Fernando Egidio 2");
+		aluno2.setNomeEscola("JDEV Treinamento 2");
+
+		// criação da disciplina
+		Disciplina disciplina3 = new Disciplina();
+		disciplina3.setDisciplina("Curso de Java 2");
+		disciplina3.setNotas(notas);
+
+		aluno2.getDisciplinas().add(disciplina3);
+
+		Disciplina disciplina4 = new Disciplina();
+		disciplina4.setDisciplina("Lógica de programação 2");
+		disciplina4.setNotas(notasLogica);
+
+		aluno2.getDisciplinas().add(disciplina4);
+
+		Aluno[] arrayAlunos = new Aluno[2];
+
+		arrayAlunos[0] = aluno;
+		arrayAlunos[1] = aluno2;
+
+		for (int i = 0; i < arrayAlunos.length; i++) {
+			System.out.println("Nome do aluno é: " + arrayAlunos[i].getNome());
+
+			for (Disciplina d : arrayAlunos[i].getDisciplinas()) {
+				System.out.println("Nome da disciplina é : " + d.getDisciplina());
+
+				for (int posNota = 0; posNota < d.getNotas().length; posNota++) {
+					System.out.println("A nota número : " + posNota + " é igual = " + d.getNotas()[posNota]);
 				}
 			}
-			System.out.println("A menor nota da disciplina = " + d.getDisciplina() + " e de valor: " + notaMin);
-			
 		}
-		
+
 	}
 }
