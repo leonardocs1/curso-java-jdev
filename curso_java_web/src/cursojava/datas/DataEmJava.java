7b150db6c83cee181678dc00a5eab4ac10cd58ba
Leonardo Costa Santos <31082603+leonardocs1@users.noreply.github.com>
2022-09-26 21:15:23 -0300

    Conhecendo o Objeto Date

---
+++ curso_java_web/src/cursojava/datas/DataEmJava.java
@@ -0,0 +1,26 @@
+package cursojava.datas;
+
+import java.util.Date;
+
+public class DataEmJava {
+
+	public static void main(String[] args) {
+		
+		Date date = new Date();
+		
+		System.out.println("Data em milisegundos: " + date.getTime());
+
+		
+		System.out.println("Dia do mês: " + date.getDate());
+		
+		System.out.println("Dia da semana: " + date.getDay());
+		
+		System.out.println("Hora do dia: " + date.getHours());
+		
+		System.out.println("Minuto da hora: " + date.getMinutes());
+		
+		System.out.println("Segundos: " + date.getSeconds());
+		
+		System.out.println("Ano: " + (date.getYear() + 1900));
+	}
+}
