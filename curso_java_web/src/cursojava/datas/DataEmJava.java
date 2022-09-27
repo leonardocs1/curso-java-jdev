847dca4cd8af275458ee1273e4d3616943c5b219
Leonardo Costa Santos <31082603+leonardocs1@users.noreply.github.com>
2022-09-26 22:41:28 -0300

    After e Before comparando datas menores e maiores

--- curso_java_web/src/cursojava/datas/DataEmJava.java
+++ curso_java_web/src/cursojava/datas/DataEmJava.java
@@ -2,53 +2,25 @@ package cursojava.datas;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 
 public class DataEmJava {
 
 	public static void main(String[] args) throws ParseException {
 		
-		Calendar calendar = Calendar.getInstance();
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
 		
-		Date date = new Date();
+		Date dataVencimentoBoleto = simpleDateFormat.parse("11/04/2021");
 		
-		System.out.println("Data em milisegundos: " + date.getTime());
-		System.out.println("Calendar em milisegundos: " + calendar.getTimeInMillis());
+		Date dataAtualHoje = simpleDateFormat.parse("07/04/2021");
 		
-		System.out.println("Dia do mês: " + date.getDate());
-		System.out.println("Calendar do mês: " + calendar.get(Calendar.DAY_OF_MONTH));
+		// After se data1 é maior que data2
+		// before se data1 é menor que data2
 		
-		System.out.println("Dia da semana: " + date.getDay());
-		System.out.println("Calendar da semana: " + (calendar.get(Calendar.DAY_OF_WEEK) -1));
-		
-		System.out.println("Hora do dia: " + date.getHours());	
-		System.out.println("Calendar Hora do dia: " + calendar.get(Calendar.HOUR_OF_DAY) );
-		
-		System.out.println("Minuto da hora: " + date.getMinutes());
-		System.out.println("Calendar Minuto da hora: " + calendar.get(Calendar.MINUTE));
-		
-		System.out.println("Segundos: " + date.getSeconds());
-		System.out.println("Calendar Segundos: " + calendar.get(Calendar.SECOND));
-		
-		System.out.println("Ano: " + (date.getYear() + 1900));
-		System.out.println("Calender Ano: " + calendar.get(Calendar.YEAR));
-		
-		//---------------------- Simple Date Format --------------------------
-		
-		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm.ss");
-		
-		System.out.println("Data atual em formato padrão e String: " + simpleDateFormat.format(date));
-		System.out.println("Calendar Data atual em formato padrão e String: " + simpleDateFormat.format(calendar.getTime()));
-		
-		
-		simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm.ss");
-		System.out.println("Data em formato para banco de dados: " + simpleDateFormat.format(date));
-		
-		simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-		System.out.println("Objeto Date: " + simpleDateFormat.parse("2020-12-31"));
-		
-		simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
-		System.out.println("Objeto Date: " + simpleDateFormat.parse("01/01/2022"));
+		if (dataAtualHoje.before(dataVencimentoBoleto)) {
+			System.out.println("Boleto não vencido");
+		} else {
+			System.out.println("Boleto vencido - URGENTE");
+		}
 	}
 }
