738c0dfd8732eb4bc5f27c3ccbdf8db852ce2132
Leonardo Costa Santos <31082603+leonardocs1@users.noreply.github.com>
2022-09-28 17:48:43 -0300

    Adicionando nova Célula no Excel

---
+++ java-jdev/src/cursojava/arquivos/ApachePoiEditando.java
@@ -0,0 +1,48 @@
+package cursojava.arquivos;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+
+public class ApachePoiEditando {
+
+	public static void main(String[] args) throws IOException {
+	
+		File file = new File("C:\\projetos\\dev\\curso-java-jdev\\java-jdev\\src\\cursojava\\arquivos\\arquivo_excel.xls");
+		
+		FileInputStream entrada = new FileInputStream(file);
+		
+		HSSFWorkbook hssfWorkbook = new HSSFWorkbook(entrada); // prepara a entrada do arquivi xls excel
+		HSSFSheet planilha = hssfWorkbook.getSheetAt(0); // pega a planilha
+		
+		Iterator<Row> linhaIterator = planilha.iterator();
+		
+		while (linhaIterator.hasNext()) { // enquanto tiver linha
+			Row linha = linhaIterator.next();
+			
+			int numeroCelulas = linha.getPhysicalNumberOfCells();
+			
+			Cell cell = linha.createCell(numeroCelulas);
+			cell.setCellValue("5.487,20");
+		}
+		
+		entrada.close();
+		
+		FileOutputStream saida = new FileOutputStream(file);
+		hssfWorkbook.write(saida);
+		saida.flush();
+		saida.close();
+		
+		System.out.println("Planilha atualizada!");
+
+	}
+
+}
